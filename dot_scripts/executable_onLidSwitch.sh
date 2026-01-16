89e3c0b52d868b2ba818d81dd4de9eea8345cbde
RichardDorian <richard.dorian@outlook.com>
2026-01-16 20:57:40 +0100

    fix(monitors): fix built-in monitor turning back on on config reload

--- dot_scripts/executable_onLidSwitch.sh
+++ dot_scripts/executable_onLidSwitch.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env sh
 
-if [ $1 = "close" ]; then
+
+status="$1"
+
+if [ "$#" -ne 1 ]; then
+  status=$(awk '{print $2}' /proc/acpi/button/lid/LID*/state)
+fi
+
+echo $status
+
+if [ "$status" = "closed" ]; then
   hyprctl keyword monitor "eDP-1, disable"
 fi
 
-if [ $1 = "open" ]; then
+if [ "$status" = "open" ]; then
   hyprctl keyword monitor "eDP-1, 1920x1200@60, 0x0, 1"
 fi
-
